da69df1b037ba2fc3e0d40c9c737321406177f62
Jacob Gardner <juhucub@gmail.com>
2025-05-31 13:45:28 -0700

    basic express tshirt API

---
+++ index.js
@@ -0,0 +1,34 @@
+
+const app = require('express')();   //function represents API were building
+const PORT = 8080;
+
+
+/* Callback function as second arg, run this function when route requested */
+app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        tshirt: '👚',
+        size: 'small'
+    })    //Send status to client
+});
+
+/* Dynamic URL parameter : represents the ID of the tshirt */
+/* Doesnt work as we need middleware, body needs to be parsed => json */
+app.post('/tshirt/:id', (req, res) => {
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if(!logo) {
+        res.status(418).send({
+            message: "We need a logo!"
+        })
+    }
+    res.send({
+        tshirt: `👚 with your logo ${logo} and ID of ${id}`
+    })
+});
+
+app.listen(
+    PORT,
+    () => console.log(`its alive on http://localhost:${PORT}`)  //backticks instead of quotes for ${PORT}
+)
+
